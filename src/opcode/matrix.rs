49ae5c61ed7d1472026be7426ae59f31fe5745a6
RinHizakura <s921975628@gmail.com>
2022-07-04 23:25:57 +0800

    Complete the simple case of matrix rotation

--- src/opcode/matrix.rs
+++ src/opcode/matrix.rs
@@ -4,6 +4,12 @@ use cgmath::*;
 
 type Result<T> = std::result::Result<T, MGLError>;
 
+fn matrix_update() -> Result<()> {
+    let ctx = mgl::ctx()?;
+    ctx.matrix_model_projection_updated = ctx.matrix_mode <= 1;
+    Ok(())
+}
+
 pub fn op_matrix_mode(mode: u8) -> Result<()> {
     let ctx = mgl::ctx()?;
 
@@ -19,7 +25,7 @@ pub fn op_load_identity() -> Result<()> {
     let _m = ctx.matrix_stack[mode].pop().ok_or(MGLError::EINVALID)?;
     ctx.matrix_stack[mode].push(Matrix4::identity());
 
-    ctx.matrix_model_projection_updated = ctx.matrix_mode <= 1;
+    matrix_update()?;
     Ok(())
 }
 
@@ -30,18 +36,19 @@ pub fn op_push_matrix() -> Result<()> {
     let m = stack.top().ok_or(MGLError::EINVALID)?;
     stack.push(m);
 
-    ctx.matrix_model_projection_updated = ctx.matrix_mode <= 1;
+    matrix_update()?;
     Ok(())
 }
 
 pub fn op_rotate(angle: f32, x: f32, y:f32, z:f32) -> Result<()> {
+    let ctx = mgl::ctx()?;
 
     let dir_code = ((x != 0.0) as usize) << 2 |
                    ((y != 0.0) as usize) << 1 |
                    ((z != 0.0) as usize) << 0;
 
     // http://en.wikipedia.org/wiki/Rotation_matrix#Basic_rotations
-    let (angle_deg, m);
+    let (angle_deg, m2);
     match dir_code {
         1 => {
             if z < 0.0 {
@@ -51,7 +58,7 @@ pub fn op_rotate(angle: f32, x: f32, y:f32, z:f32) -> Result<()> {
             {
                 angle_deg = Deg(angle);
             }
-            m = Matrix4::from_angle_z(angle_deg);
+            m2 = Matrix4::from_angle_z(angle_deg);
         }
         2 => {
             if y < 0.0 {
@@ -61,7 +68,7 @@ pub fn op_rotate(angle: f32, x: f32, y:f32, z:f32) -> Result<()> {
             {
                 angle_deg = Deg(angle);
             }
-            m = Matrix4::from_angle_y(angle_deg);
+            m2 = Matrix4::from_angle_y(angle_deg);
         }
         4 => {
             if x < 0.0 {
@@ -71,11 +78,17 @@ pub fn op_rotate(angle: f32, x: f32, y:f32, z:f32) -> Result<()> {
             {
                 angle_deg = Deg(angle);
             }
-            m = Matrix4::from_angle_x(angle_deg);
+            m2 = Matrix4::from_angle_x(angle_deg);
         }
         // TODO: only simple case of rotation is supported now
         _ => todo!(),
     }
 
+    let mode = ctx.matrix_mode as usize;
+    let stack = &mut ctx.matrix_stack[mode];
+    let m1 = stack.pop().ok_or(MGLError::EINVALID)?;
+    stack.push(m1 * m2);
+
+    matrix_update()?;
     Ok(())
 }
